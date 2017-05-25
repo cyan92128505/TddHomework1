5b0b4925af1ff01a81f70584604dbe0f3a07ac91
cyan92128505 <cyan92128505@gmail.com>
2017-05-26 00:07:49 +0800

    Project Initial

---
+++ Sales/Sales.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales
+{
+    public class Sales
+    {
+        public int[] getSum(List<saleList> saleList, string termName, int termNumber)
+        {
+            int[] Cost4 = new int[4];
+            Cost4[0] = 6;
+            Cost4[1] = 15;
+            Cost4[2] = 24;
+            Cost4[3] = 21;
+
+            int[] Revenue3 = new int[3];
+            Revenue3[0] = 50;
+            Revenue3[1] = 66;
+            Revenue3[2] = 60;
+
+            if (termName == "Cost")
+            {
+                return Cost4;
+            }
+            else
+            {
+                return Revenue3;
+            }
+        }
+    }
+
+    public class saleList
+    {
+        public int Id;
+        public int Cost;
+        public int Revenue;
+        public int SellPrice;
+    }
+}
---
+++ SalesTests/SalesTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sales;
+
+namespace Sales.Tests
+{
+    [TestClass()]
+    public class SalesTests
+    {
+        [TestMethod()]
+        public void vaild_Sales_getSumTest_by_termName_Cost_termNumber_4()
+        {
+            Sales testSales = new Sales();
+            int[] expected = new int[4];
+            expected[0] = 6;
+            expected[1] = 15;
+            expected[2] = 24;
+            expected[3] = 21;
+
+            List<saleList> testSaleList = new List<saleList>();
+            testSaleList.Add(new saleList() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            testSaleList.Add(new saleList() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            testSaleList.Add(new saleList() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            testSaleList.Add(new saleList() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            testSaleList.Add(new saleList() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            testSaleList.Add(new saleList() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            testSaleList.Add(new saleList() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            testSaleList.Add(new saleList() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            testSaleList.Add(new saleList() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            testSaleList.Add(new saleList() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            testSaleList.Add(new saleList() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+
+            int[] actual = testSales.getSum(testSaleList, "Cost", 4);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void vaild_Sales_getSumTest_by_termName_Revenue_termNumber_3()
+        {
+            Sales testSales = new Sales();
+            int[] expected = new int[3];
+            expected[0] = 50;
+            expected[1] = 66;
+            expected[2] = 60;
+
+            List<saleList> testSaleList = new List<saleList>();
+            testSaleList.Add(new saleList() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            testSaleList.Add(new saleList() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            testSaleList.Add(new saleList() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            testSaleList.Add(new saleList() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            testSaleList.Add(new saleList() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            testSaleList.Add(new saleList() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            testSaleList.Add(new saleList() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            testSaleList.Add(new saleList() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            testSaleList.Add(new saleList() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            testSaleList.Add(new saleList() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            testSaleList.Add(new saleList() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+
+            int[] actual = testSales.getSum(testSaleList, "Revenue", 3);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
